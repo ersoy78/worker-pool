86109f5e1c8b880dba9adc9b7121010a91c95c25
e <e@e>
2023-09-11 21:15:56 +1000

    Added TS Types

---
+++ workerpool.d.ts
@@ -0,0 +1,38 @@
+import { EventEmitter } from "events";
+
+export interface WorkerPoolOptions {
+  poolSize: number;
+  workerFunction: (task: any) => Promise<any>;
+  taskList: any[];
+  retry?: boolean;
+  delay?: number;
+}
+
+export interface WorkerPoolResults {
+  ok: any[];
+  fail: { item: any; error: Error }[];
+}
+
+export class WorkerPool extends EventEmitter {
+  constructor(options: WorkerPoolOptions);
+
+  info(): void;
+
+  add(worker: any): void;
+
+  push(task: any, unique?: boolean): void;
+
+  isQueued(worker: any): boolean;
+
+  list(): any[];
+
+  isWorking(): boolean;
+
+  remove(worker: any): void;
+
+  idle(): void;
+
+  stopIdle(): void; // Renamed 'off' to 'stopIdle'
+
+  start(): Promise<WorkerPoolResults>;
+}
